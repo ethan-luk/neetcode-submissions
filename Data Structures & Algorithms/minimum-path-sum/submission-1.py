409fd7c86567fb1ec0f5ceb52bec53a023b3f784
Ethan Luk <100002960+ethan-luk@users.noreply.github.com>
2026-08-10 09:13:53 -0400

    Add: minimum-path-sum - submission-1

---
+++ Data Structures & Algorithms/minimum-path-sum/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = [[float('inf')] * (COLS + 1) for _ in range(ROWS + 1)]
+        dp[ROWS-1][COLS-1] = grid[ROWS-1][COLS-1]
+        dp[ROWS][COLS - 1] = 0
+        dp[ROWS - 1][COLS] = 0
+
+        for i in range(ROWS - 1, -1, -1):
+            for j in range(COLS - 1, -1, -1):
+                dp[i][j] = grid[i][j] + min(dp[i+1][j], dp[i][j+1])
+        
+        return dp[0][0]
